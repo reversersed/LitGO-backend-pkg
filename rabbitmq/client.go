24d8d991e672447c4395bf2747a6c5471eb84edd
reversersed <myregisg@gmail.com>
2025-03-17 22:22:56 +0300

    update package

--- rabbitmq/client.go
+++ rabbitmq/client.go
@@ -1,14 +1,15 @@
 package rabbitmq
 
 import (
+	"context"
 	"fmt"
+	"io"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type rabbitClient struct {
-	*amqp.Connection
-}
+//go:generate mockgen -source=init.go -destination=mocks/rabbit.go
 type RabbitConfig struct {
 	Rabbit_User string `env:"RABBITMQ_USER" env-required:"true" env-description:"RabbitMQ user"`
 	Rabbit_Pass string `env:"RABBITMQ_PASS" env-required:"true" env-description:"RabbitMQ password"`
@@ -16,10 +17,38 @@ type RabbitConfig struct {
 	Rabbit_Port string `env:"RABBITMQ_PORT" env-required:"true" env-description:"RabbitMQ port"`
 }
 
-func New(config *RabbitConfig) (*rabbitClient, error) {
+type logger interface {
+	Info(...any)
+	Infof(string, ...any)
+	Errorf(string, ...any)
+}
+type storage interface {
+	IncreateBookCount(context.Context, primitive.ObjectID) error
+}
+type RabbitService struct {
+	conn     *amqp.Connection
+	logger   logger
+	storage  storage
+	channels []io.Closer
+}
+
+func New(config *RabbitConfig, logger logger, storage storage) (*RabbitService, error) {
 	connection, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", config.Rabbit_User, config.Rabbit_Pass, config.Rabbit_Host, config.Rabbit_Port))
 	if err != nil {
 		return nil, err
 	}
-	return &rabbitClient{connection}, nil
+	return &RabbitService{
+		conn:    connection,
+		logger:  logger,
+		storage: storage,
+	}, nil
+}
+
+func (s *RabbitService) Close() error {
+	for _, c := range s.channels {
+		if err := c.Close(); err != nil {
+			return err
+		}
+	}
+	return s.conn.Close()
 }
