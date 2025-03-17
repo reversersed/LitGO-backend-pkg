d6f20c10abb65aa9ad8736029c7b3a675cf767af
reversersed <myregisg@gmail.com>
2025-03-17 22:52:11 +0300

    revoke pkg

--- rabbitmq/client.go
+++ rabbitmq/client.go
@@ -1,14 +1,14 @@
 package rabbitmq
 
 import (
-	"context"
 	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-//go:generate mockgen -source=client.go -destination=mocks/rabbit.go
+type rabbitClient struct {
+	*amqp.Connection
+}
 type RabbitConfig struct {
 	Rabbit_User string `env:"RABBITMQ_USER" env-required:"true" env-description:"RabbitMQ user"`
 	Rabbit_Pass string `env:"RABBITMQ_PASS" env-required:"true" env-description:"RabbitMQ password"`
@@ -16,59 +16,10 @@ type RabbitConfig struct {
 	Rabbit_Port string `env:"RABBITMQ_PORT" env-required:"true" env-description:"RabbitMQ port"`
 }
 
-type logger interface {
-	Info(...any)
-	Infof(string, ...any)
-	Errorf(string, ...any)
-}
-type storage interface {
-	IncreateBookCount(context.Context, primitive.ObjectID) error
-}
-type RabbitService struct {
-	Conn     *amqp.Connection
-	Logger   logger
-	Storage  storage
-	Channels []*amqp.Channel
-}
-
-func New(config *RabbitConfig, logger logger, storage storage) (*RabbitService, error) {
+func New(config *RabbitConfig) (*rabbitClient, error) {
 	connection, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", config.Rabbit_User, config.Rabbit_Pass, config.Rabbit_Host, config.Rabbit_Port))
 	if err != nil {
 		return nil, err
 	}
-	return &RabbitService{
-		Conn:    connection,
-		Logger:  logger,
-		Storage: storage,
-	}, nil
-}
-func (s *RabbitService) CreateChannel() (*amqp.Channel, error) {
-	channel, err := s.Conn.Channel()
-	if err != nil {
-		return nil, err
-	}
-	s.Channels = append(s.Channels, channel)
-	return channel, nil
-}
-func (s *RabbitService) CloseChannel(channel *amqp.Channel) {
-	ChannelsCopy := make([]*amqp.Channel, len(s.Channels))
-	copy(ChannelsCopy, s.Channels)
-
-	s.Channels = make([]*amqp.Channel, 0)
-	for _, ch := range ChannelsCopy {
-		if ch == channel {
-			continue
-		}
-		s.Channels = append(s.Channels, ch)
-	}
-
-	channel.Close()
-}
-func (s *RabbitService) Close() error {
-	for _, c := range s.Channels {
-		if err := c.Close(); err != nil {
-			return err
-		}
-	}
-	return s.Conn.Close()
+	return &rabbitClient{connection}, nil
 }
